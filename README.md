051d494d5c9bb570501e8af4aeaa3fb45121cd5e
Aumarh <aumarh@riseup.net>
2022-05-03 19:31:12 +0200

    Add first working version

--- README.md
+++ README.md
@@ -3,13 +3,16 @@
 When a program is not run with any input, print a random color:
 
 ```bash
-$node index.js
+$ node index.js color name
+print '#' in color
 
+$ node index.js ask
+specify color
 
-$node index.js
-
+$ node index.js
+random color
 ```
 
-- Without any user input: print random color
-- With user input of a correct color name: print color
-- With user input of an incorrect color name: print error message that color doesn't exist
+- With user input of with correct color name: print a block of approximately 31x9 #
+- With user input and no specified color: print message asking user for color
+- With no user input: print block with random color
--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-console.log('Hello World');
+// import colors from library
+import chalk from 'chalk';
+import randomColor from 'randomcolor';
+
+const color = randomColor();
+
+const hashBlock = `##############################
+##############################
+##############################
+#####                   ######
+#####      ${color}             ######
+#####                   ######
+##############################
+##############################
+##############################`;
+
+if (process.argv.length < 3) {
+  console.log(chalk.hex(color)(hashBlock));
+} else if (process.argv[2] == 'ask') {
+  console.log('Specify color');
+} else {
+  console.log(
+    chalk.hex(
+      randomColor({ luminosity: process.argv[3], hue: process.argv[2] }),
+    )(hashBlock),
+  );
+}
